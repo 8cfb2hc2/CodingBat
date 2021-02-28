ddb4018862e55d5b5c49721a555d0f32a59c48cd
8cfb2hc2 <79760900+8cfb2hc2@users.noreply.github.com>
2021-02-28 13:21:00 +0100

    first commit

---
+++ ex8string.java
@@ -0,0 +1,12 @@
+/*
+	
+Given a string, return true if it ends in "ly".
+
+
+endsLy("oddly") → true
+endsLy("y") → false
+endsLy("oddy") → false
+*/
+public boolean endsLy(String str) {
+  return (str.length() >= 2) ? str.substring(str.length()-2,str.length()).equals("ly"): false;
+}
